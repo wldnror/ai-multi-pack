008d0fda5535fde2a62cb8fd5c94e17a4fb007a5
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-07 22:18:45 +0900

    Update udp.py

--- udp.py
+++ udp.py
@@ -1,17 +1,18 @@
 import socket
 
 udp_ip = "0.0.0.0"  # 모든 인터페이스에서 들어오는 데이터를 수신
-udp_port = 12345  # 안드로이드 앱과 동일한 포트 번호 사용
+udp_port = 12345
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP 소켓 생성
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((udp_ip, udp_port))
 
 print("UDP 서버가 시작되었습니다. 대기 중...")
 
 while True:
-    data, addr = sock.recvfrom(1024)  # 버퍼 크기는 1024 바이트
-    print(f"수신된 메시지: {data} from {addr}")
+    data, addr = sock.recvfrom(1024)
+    print(f"수신된 메시지: {data.decode()} from {addr}")
 
-    # 메시지를 받은 후 응답 보내기
-    response_message = "응답 메시지"
-    sock.sendto(response_message.encode(), addr)
+    # 클라이언트(안드로이드 앱)에게 라즈베리 파이의 IP 주소를 보냄
+    if data:
+        response = f"My IP address is {addr[0]}"
+        sock.sendto(response.encode(), addr)
