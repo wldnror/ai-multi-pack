652651b88de4b4a8077659b6a3e34b69f1c89e4e
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-07 21:00:57 +0900

    Update udp.py

--- udp.py
+++ udp.py
@@ -1,38 +1,13 @@
 import socket
-import dns.resolver
-import dns.exception
-import dns.query
-import dns.tsigkeyring
-import dns.update
-import dns.zone
 
-def register_service(service_name, port):
-    # DNS 업데이트
-    update = dns.update.Update('local.')
-    update.add('_my_udp_service._udp', 60, 'SRV', f'0 0 {port} {socket.gethostname()}.local.')
+udp_ip = "0.0.0.0"  # 모든 인터페이스에서 들어오는 데이터를 수신
+udp_port = 12345  # 안드로이드 앱과 동일한 포트 번호 사용
 
-    try:
-        # mDNS 서버로 업데이트 전송
-        response = dns.query.udp(update, '224.0.0.251')
-        print(response)
-    except dns.exception.Timeout:
-        print('mDNS update timeout')
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP 소켓 생성
+sock.bind((udp_ip, udp_port))
 
-# UDP 서버를 실행할 호스트와 포트
-HOST = '0.0.0.0'  # 모든 네트워크 인터페이스에서 수신
-PORT = 12345      # 서비스를 제공할 포트
+print("UDP 서버가 시작되었습니다. 대기 중...")
 
-# 소켓 생성 (IPv4, UDP)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((HOST, PORT))
-
-print(f'Server is listening on {HOST}:{PORT}')
-
-# 서비스 등록
-register_service('_my_udp_service._udp', PORT)
-
-# 무한 루프로 UDP 메시지 수신 대기
 while True:
-    # 데이터와 클라이언트 주소 수신
-    data, client_address = server_socket.recvfrom(1024)
-    print(f'Received message from {client_address}: {data.decode()}')
+    data, addr = sock.recvfrom(1024)  # 버퍼 크기는 1024 바이트
+    print(f"수신된 메시지: {data} from {addr}")
