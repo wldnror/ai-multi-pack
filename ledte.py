bc6eb6975ae1f91274b3c002eb925fe85998ed34
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-14 14:44:42 +0900

    Update ledte.py

--- ledte.py
+++ ledte.py
@@ -5,11 +5,11 @@ import sounddevice as sd
 import time
 
 # LED 스트립 설정
-LED_COUNT = 150
-LED_PIN = board.D21
-LED_BRIGHTNESS = 0.5
-SAMPLE_RATE = 48000
-FFT_SIZE = 1024
+LED_COUNT = 330      # LED 개수
+LED_PIN = board.D21  # GPIO 핀 번호
+LED_BRIGHTNESS = 0.5 # LED 밝기 (0.0에서 1.0 사이)
+SAMPLE_RATE = 48000  # 오디오 샘플레이트
+FFT_SIZE = 1024      # FFT 크기
 
 # NeoPixel 객체 초기화
 strip = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_write=False)
@@ -18,11 +18,11 @@ strip = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, auto_wr
 def control_leds(fft_results):
     max_val = np.max(fft_results) if np.max(fft_results) != 0 else 1
     scaled_fft = 255 * (fft_results / max_val)  # 결과를 0~255 범위로 스케일
-    num_bands = 10  # 더 세분화된 대역
+    num_bands = 11  # 새로운 줄 개수
     leds_per_band = LED_COUNT // num_bands
 
     for i in range(num_bands):
-        hue = int(120 * scaled_fft[i] / 255)  # Hue 값을 120에서 0으로 (초록에서 빨강으로)
+        hue = int(120 * scaled_fft[i] / 255)  # Hue 값 (색상의 각도)
         brightness = int(scaled_fft[i])
         color = hsv_to_rgb(hue, 255, brightness)
         for j in range(leds_per_band):
@@ -46,7 +46,7 @@ def audio_callback(indata, frames, time, status):
     if status:
         print("Status:", status)
     fft_result = np.abs(np.fft.rfft(indata[:, 0], n=FFT_SIZE))
-    fft_result_split = np.array_split(fft_result, 10)
+    fft_result_split = np.array_split(fft_result, num_bands)
     fft_result_means = [np.mean(part) for part in fft_result_split]
     control_leds(fft_result_means)
 
