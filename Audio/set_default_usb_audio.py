5a1843334d968e144f27499d9fc5281b7f32b26f
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-21 02:47:26 +0900

    Update and rename test.py to set_default_usb_audio.py

--- Audio/set_default_usb_audio.py
+++ Audio/set_default_usb_audio.py
@@ -4,7 +4,6 @@ pulse = pulsectl.Pulse('audio-manager')
 
 def set_default_usb_speaker():
     sinks = pulse.sink_list()
-    # 이름에 'usb'가 포함되고, 설명에 오디오 출력과 관련된 키워드가 포함된 싱크를 찾습니다.
     audio_keywords = ['Stereo', 'Speaker', 'Audio', 'Analog']
     usb_speakers = [
         sink for sink in sinks
@@ -13,7 +12,17 @@ def set_default_usb_speaker():
     if usb_speakers:
         pulse.default_set(usb_speakers[0])
         print(f"Default audio device set to USB speaker: {usb_speakers[0].description}")
+        # 기본 장치로 모든 스트림 이동
+        move_other_streams(usb_speakers[0].index)
     else:
         print("No USB speakers found.")
+
+def move_other_streams(new_default_sink_index):
+    sink_inputs = pulse.sink_input_list()
+    for stream in sink_inputs:
+        # 'loopback'으로 시작하는 스트림을 제외하고 모든 스트림 이동
+        if not stream.name.startswith('loopback'):
+            pulse.sink_input_move(stream.index, new_default_sink_index)
+            print(f"Moved stream {stream.name} to new default sink")
 
 set_default_usb_speaker()
