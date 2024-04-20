b8fa67ac0f74d438c2645e75a69edf81b94fedb2
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-21 03:12:59 +0900

    Update main.py

--- Audio/main.py
+++ Audio/main.py
@@ -1,20 +1,13 @@
 import subprocess
 
 def run_scripts():
-    # 스크립트 경로 설정
-    script1 = 'Audio/set_default_usb_audio.py'
-    script2 = 'Audio/loopback_sink_mover.py'
+    # 첫 번째 스크립트 실행
+    subprocess.run(['python', 'Audio/loopback_sink_mover.py'], check=True)
+    print("첫 번째 스크립트 실행 완료")
 
-    # 두 스크립트를 동시에 실행하기 위해 subprocess.Popen을 사용
-    processes = []
-    processes.append(subprocess.Popen(['python', script1]))
-    processes.append(subprocess.Popen(['python', script2]))
+    # 두 번째 스크립트 실행
+    subprocess.run(['python', 'Audio/set_default_usb_audio.py'], check=True)
+    print("두 번째 스크립트 실행 완료")
 
-    # 모든 스크립트가 종료될 때까지 기다림
-    for process in processes:
-        process.wait()
-
-    print("모든 스크립트 실행 완료")
-
-# 스크립트 실행
-run_scripts()
+if __name__ == "__main__":
+    run_scripts()
