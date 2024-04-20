d064862d516836e20695d4afa3cb0993d46f24fc
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-21 03:51:50 +0900

    Update bluetooth.py

--- Audio/bluetooth.py
+++ Audio/bluetooth.py
@@ -1,6 +1,16 @@
 import subprocess
 import time
 
+def any_device_connected():
+    # bluetoothctl을 사용하여 모든 연결된 장치를 확인
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    process.stdin.write('info\n')
+    process.stdin.write('exit\n')
+    out, err = process.communicate()
+    if "Connected: yes" in out:
+        return True
+    return False
+
 def get_last_connected_device():
     # bluetoothctl을 통해 페어링된 장치 목록을 가져옴
     process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
@@ -17,19 +27,12 @@ def get_last_connected_device():
                     return mac_address
     return None
 
-def is_device_connected(device_address):
-    # 블루투스 장치의 연결 상태 확인
-    result = subprocess.run(['bluetoothctl', 'info', device_address], capture_output=True, text=True)
-    if "Connected: yes" in result.stdout:
-        return True
-    return False
-
 def connect_bluetooth_device(device_address):
-    # 블루투스 장치 연결 시도 전에 연결 상태 확인
+    # 블루투스 장치 연결 시도 전에 전체 연결 상태 확인
+    if any_device_connected():
+        print("A device is already connected.")
+        return
     while True:  # 무한 반복
-        if is_device_connected(device_address):
-            print(f"Device {device_address} is already connected.")
-            break
         try:
             result = subprocess.run(['bluetoothctl', 'connect', device_address], capture_output=True, text=True)
             if "Connection successful" in result.stdout:
@@ -43,9 +46,12 @@ def connect_bluetooth_device(device_address):
             time.sleep(10)  # 예외 발생시에도 10초 후 재시도
 
 if __name__ == '__main__':
-    device_address = get_last_connected_device()
-    if device_address:
-        print(f"Attempting to connect to the last connected device: {device_address}")
-        connect_bluetooth_device(device_address)
+    if not any_device_connected():  # 장치 연결 상태 먼저 확인
+        device_address = get_last_connected_device()
+        if device_address:
+            print(f"Attempting to connect to the last connected device: {device_address}")
+            connect_bluetooth_device(device_address)
+        else:
+            print("No paired devices found.")
     else:
-        print("No paired devices found.")
+        print("A device is already connected, no need to reconnect.")
