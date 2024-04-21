7d9c0438606ca71f524807246f14b8effb6ce6d6
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-21 14:47:41 +0900

    Update 1.py

--- black_box/1.py
+++ black_box/1.py
@@ -1,6 +1,17 @@
+import cv2  # cv2 모듈을 불러옵니다.
+
+# 비디오 캡처 객체를 생성합니다. 여기서 '0'은 시스템의 기본 카메라를 의미합니다.
+cap = cv2.VideoCapture(0)
+
+# cap 객체의 초기화 상태를 확인합니다.
 if cap.isOpened():
-    # 지원하는 해상도 및 프레임레이트 출력
+    print("카메라가 성공적으로 열렸습니다.")
+    # 지원하는 해상도 및 프레임 레이트를 출력합니다.
     print("Width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     print("Height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     print("FPS: ", cap.get(cv2.CAP_PROP_FPS))
+else:
+    print("카메라를 열 수 없습니다.")
+
+# 비디오 캡처 객체를 해제합니다.
 cap.release()
