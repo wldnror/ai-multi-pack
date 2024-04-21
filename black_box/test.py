280eca6fa520801cc4af3532052be9f88c69b876
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-22 00:24:44 +0900

    Update test.py

--- black_box/test.py
+++ black_box/test.py
@@ -5,7 +5,7 @@ import numpy as np
 net = cv2.dnn.readNet('/home/user/LED/black_box/yolov4.weights', '/home/user/LED/black_box/yolov4.cfg')
 
 classes = []
-with open("coco.names", "r") as f:
+with open("/home/user/LED/black_box/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
