00bc405e2191097a22a9c91768b01912494007da
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-21 12:21:15 +0900

    Update bluetooth.py

--- Audio/bluetooth.py
+++ Audio/bluetooth.py
@@ -1,38 +1,46 @@
 import subprocess
 import time
 
-def check_device_connected(device_address):
-    # 특정 장치의 연결 상태를 확인
-    result = subprocess.run(['bluetoothctl', 'info', device_address], capture_output=True, text=True)
-    return "Connected: yes" in result.stdout
-
 def get_last_connected_device():
-    # 가장 최근에 연결된 블루투스 장치의 MAC 주소를 가져옴
-    result = subprocess.run(['bluetoothctl', 'devices'], capture_output=True, text=True)
-    lines = result.stdout.strip().split('\n')
-    for line in reversed(lines):
-        if "Device" in line:
-            parts = line.split()
-            if len(parts) >= 2:
-                return parts[1]
+    # bluetoothctl을 통해 페어링된 장치 목록을 가져옴
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    process.stdin.write('devices\n')
+    process.stdin.write('exit\n')
+    out, err = process.communicate()
+    if out:
+        lines = out.strip().split('\n')
+        for line in reversed(lines):
+            if "Device" in line:
+                parts = line.split()
+                if len(parts) >= 2:
+                    mac_address = parts[1]  # MAC 주소 추출
+                    return mac_address
     return None
 
+def is_device_connected(device_address):
+    # 블루투스 장치의 연결 상태 확인
+    result = subprocess.run(['bluetoothctl', 'info', device_address], capture_output=True, text=True)
+    if "Connected: yes" in result.stdout:
+        return True
+    return False
+
 def connect_bluetooth_device(device_address):
-    # 블루투스 장치 연결 시도
-    if check_device_connected(device_address):
-        print("A device is already connected.")
-        return
-    while True:
-        result = subprocess.run(['bluetoothctl', 'connect', device_address], capture_output=True, text=True)
-        if "Connection successful" in result.stdout:
-            if check_device_connected(device_address):
+    # 블루투스 장치 연결 시도 전에 연결 상태 확인
+    while True:  # 무한 반복
+        if is_device_connected(device_address):
+            print(f"Device {device_address} is already connected.")
+            break
+        try:
+            result = subprocess.run(['bluetoothctl', 'connect', device_address], capture_output=True, text=True)
+            if "Connection successful" in result.stdout:
                 print("Connected to the device successfully.")
                 break
             else:
                 print("Connection failed. Retrying...")
-        else:
-            print(f"Connection failed: {result.stderr.strip()}")
-        time.sleep(10)
+                time.sleep(10)  # 10초 후 재시도
+        except Exception as e:
+            print(f"Error connecting to the device: {str(e)}")
+            time.sleep(10)  # 예외 발생시에도 10초 후 재시도
 
 if __name__ == '__main__':
     device_address = get_last_connected_device()
