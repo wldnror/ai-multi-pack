fd11ab736d28139c8343ecb6e97075b7698b72b6
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-23 00:23:26 +0900

    Update test.py

--- black_box/test.py
+++ black_box/test.py
@@ -1,22 +1,27 @@
 import cv2
 import numpy as np
 
-# YOLO 모델 불러오기
+# YOLO 모델 및 클래스 레이블 로드
 net = cv2.dnn.readNet('/home/user/LED/black_box/yolov4.weights', '/home/user/LED/black_box/yolov4.cfg')
-
 classes = []
 with open("/home/user/LED/black_box/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
 layer_names = net.getLayerNames()
-out_layer_indices = net.getUnconnectedOutLayers()
-output_layers = [layer_names[i - 1] for i in out_layer_indices.flatten()]
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-# 카메라 캡처 시작
+# 카메라 캡처 초기화
 cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
 while True:
-    _, frame = cap.read()
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
     height, width, channels = frame.shape
 
     # 객체 탐지 수행
@@ -24,7 +29,6 @@ while True:
     net.setInput(blob)
     outs = net.forward(output_layers)
 
-    # 정보를 화면에 표시
     class_ids = []
     confidences = []
     boxes = []
@@ -34,30 +38,32 @@ while True:
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             if confidence > 0.5:
+                # 객체 탐지
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
 
+                # 사각형 좌표
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
-
                 boxes.append([x, y, w, h])
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
 
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
+    for i in indexes.flatten():
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        if label == "person" or label == "car":
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, label, (x, y + 30), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
-    
-    cv2.imshow("Image", frame)
-    key = cv2.waitKey(1)
-    if key == 27:  # ESC 키
+            cv2.putText(frame, label, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # 결과 화면에 표시
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 
+# 완료 후 자원 해제
 cap.release()
 cv2.destroyAllWindows()
