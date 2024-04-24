4e31afaf2f5d7f023b1d511555db67d0e8d64212
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-24 21:21:02 +0900

    Update 1.py

--- black_box/1.py
+++ black_box/1.py
@@ -1,10 +1,16 @@
 import cv2
 
 cap = cv2.VideoCapture(0)  # 웹캠 열기
-fps = cap.get(cv2.CAP_PROP_FPS)  # 실제 카메라의 FPS를 얻음
+
+# 실제 카메라에서 사용되는 프레임 레이트를 얻습니다.
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# 비디오 스트림의 너비와 높이를 가져옵니다.
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))  # 실제 FPS 사용
+out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))  # 실제 FPS 및 해상도 사용
 
 while True:
     ret, frame = cap.read()
