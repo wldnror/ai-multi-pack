990bae6b0385c3244d892af38baf3172de129e16
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-24 21:35:07 +0900

    Update 1.py

--- black_box/1.py
+++ black_box/1.py
@@ -1,32 +1,32 @@
 import cv2
+import time
 
-cap = cv2.VideoCapture(0)  # 웹캠 열기
-
-# 해상도를 1920x1080으로 설정
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-# 설정한 해상도를 확인
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print("Set resolution: {}x{}".format(width, height))
-
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-fps = cap.get(cv2.CAP_PROP_FPS)  # 실제 카메라의 FPS를 얻음
-out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))  # 실제 FPS 및 설정한 해상도 사용
+out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (1920, 1080))
+
+frame_count = 0
+start_time = time.time()
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    
-    # 프레임 처리 로직 (예: 객체 탐지)
 
-    out.write(frame)  # 처리된 프레임을 출력 파일에 기록
-    
-    cv2.imshow('Frame', frame)
+    frame_count += 1
+
+    # 객체 탐지 및 기타 프레임 처리 로직
+
+    out.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+elapsed_time = time.time() - start_time
+actual_fps = frame_count / elapsed_time
+print(f"Actual FPS: {actual_fps}")
 
 cap.release()
 out.release()
