5e84e4cef4121cc5d73daaa267bacfe802c412ee
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-24 18:50:40 +0900

    Update main.py

--- black_box/main.py
+++ black_box/main.py
@@ -45,7 +45,7 @@ def start_ffmpeg_recording(duration=30):
         'ffmpeg',
         '-f', 'v4l2',
         '-framerate', '30',
-        '-video_size', '4096x2160',
+        '-video_size', '1920x1080',
         '-i', '/dev/video0',
         '-c:v', 'libx264',
         '-preset', 'ultrafast',
