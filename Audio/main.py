9df5f85f6ef2b1632edbbec2a221be99e9857882
wldnror <72863449+wldnror@users.noreply.github.com>
2024-04-27 14:24:46 +0900

    Update main.py

--- Audio/main.py
+++ Audio/main.py
@@ -1,17 +1,26 @@
 import subprocess
 
 def run_scripts():
-    # snd-aloop 모듈 로드
-    subprocess.run(['sudo', 'modprobe', 'snd-aloop'], check=True)
-    print("snd-aloop 모듈 로드 완료")
+    try:
+        # snd-aloop 모듈 로드
+        subprocess.run(['sudo', 'modprobe', 'snd-aloop'], check=True)
+        print("snd-aloop 모듈 로드 완료")
+    except subprocess.CalledProcessError as e:
+        print(f"Error loading snd-aloop module: {e}")
 
-    # 첫 번째 스크립트 실행
-    subprocess.run(['python', 'Audio/loopback_sink_mover.py'], check=True)
-    print("첫 번째 스크립트 실행 완료")
+    try:
+        # 첫 번째 스크립트 실행
+        subprocess.run(['python', 'Audio/loopback_sink_mover.py'], check=True)
+        print("첫 번째 스크립트 실행 완료")
+    except subprocess.CalledProcessError as e:
+        print(f"Error in first script: {e}")
 
-    # 두 번째 스크립트 실행
-    subprocess.run(['python', 'Audio/set_default_usb_audio.py'], check=True)
-    print("두 번째 스크립트 실행 완료")
+    try:
+        # 두 번째 스크립트 실행
+        subprocess.run(['python', 'Audio/set_default_usb_audio.py'], check=True)
+        print("두 번째 스크립트 실행 완료")
+    except subprocess.CalledProcessError as e:
+        print(f"Error in second script: {e}")
 
 if __name__ == "__main__":
     run_scripts()
