ebc2cd0bd88e8eb4ec69781b1a1359db300425c4
wldnror <72863449+wldnror@users.noreply.github.com>
2024-05-01 21:35:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# main.py
-
 import socket
 import socketserver
 import subprocess
@@ -66,7 +64,7 @@ while True:
     print(f"수신된 메시지: {message} from {address}")
 
     if message == "0003":
-        if not process_exists('black_box/main.py'):
+        if not process_exists():
             print("main.py 실행 중이 아닙니다. 프로세스를 시작합니다.")
             start_process()
         else:
