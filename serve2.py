f9919c5e94043484b20cff0b85b96806fb6ea6ba
wldnror <72863449+wldnror@users.noreply.github.com>
2024-05-03 15:10:34 +0900

    Delete serve2.py

--- serve2.py
+++
@@ -1,8 +0,0 @@
-# 예시: serve2.py
-def run(conn):
-    while True:
-        message = conn.recv()
-        print(f"serve2 received: {message}")
-        # 메시지에 따라 작업 수행
-        if message == "button1":
-            print("serve2 handling button1")
