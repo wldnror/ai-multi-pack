ddd296768ddc563c545bbc7e9a0a242ebd96b190
wldnror <72863449+wldnror@users.noreply.github.com>
2024-05-06 22:11:03 +0900

    Update main.py

--- Audio/main.py
+++ Audio/main.py
@@ -1,28 +1,28 @@
 import subprocess
-import time  # time 모듈을 임포트합니다.
+import time
 
 def run_scripts():
     try:
         # snd-aloop 모듈 로드
         subprocess.run(['sudo', 'modprobe', 'snd-aloop'], check=True)
         print("snd-aloop 모듈 로드 완료")
-        time.sleep(2)  # 첫 번째 동작 후 2초 대기
+        time.sleep(1)  # 첫 번째 동작 후 2초 대기
     except subprocess.CalledProcessError as e:
         print(f"Error loading snd-aloop module: {e}")
-        time.sleep(2)  # 오류 발생 후 2초 대기
+        time.sleep(1)  # 오류 발생 후 2초 대기
 
     try:
         # 첫 번째 스크립트 실행
-        subprocess.run(['python', 'Audio/loopback_sink_mover.py'], check=True)
+        subprocess.run(['/usr/bin/python3', '/home/user/ai-multi-pack/Audio/loopback_sink_mover.py'], check=True)
         print("첫 번째 스크립트 실행 완료")
-        time.sleep(2)  # 두 번째 동작 후 2초 대기
+        time.sleep(1)  # 두 번째 동작 후 2초 대기
     except subprocess.CalledProcessError as e:
         print(f"Error in first script: {e}")
-        time.sleep(2)  # 오류 발생 후 2초 대기
+        time.sleep(1)  # 오류 발생 후 2초 대기
 
     try:
         # 두 번째 스크립트 실행
-        subprocess.run(['python', 'Audio/set_default_usb_audio.py'], check=True)
+        subprocess.run(['/usr/bin/python3', '/home/user/ai-multi-pack/Audio/set_default_usb_audio.py'], check=True)
         print("두 번째 스크립트 실행 완료")
     except subprocess.CalledProcessError as e:
         print(f"Error in second script: {e}")
