c16e6e4ebb0750295d35be663735156c485b4697
wldnror <72863449+wldnror@users.noreply.github.com>
2024-05-06 18:49:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -84,17 +84,25 @@ def start_recording():
 
 
 # 녹화 중지
-async def stop_recording():
+# 녹화 중지
+def stop_recording():
+    print("Attempting to stop recording...")
     try:
+        output = subprocess.check_output(['pgrep', '-f', 'black_box/main.py'])
+        print(f"Process found with PID: {output.decode().strip()}")
         subprocess.check_output(['pkill', '-f', 'black_box/main.py'])
         print("Recording stopped.")
-        await asyncio.sleep(1)  # 프로세스 종료를 기다림
-        await force_release_camera()  # 카메라 자원 해제를 비동기로 처리
-    except subprocess.CalledProcessError:
-        print("Recording process not found.")
-        await force_release_camera()
-    finally:
+        time.sleep(1)  # 프로세스 종료를 기다림
+        force_release_camera()  # 카메라 자원 해제 시도
         return "NOT_RECORDING"
+    except subprocess.CalledProcessError as e:
+        print(f"No recording process found: {e}")
+        force_release_camera()
+        return "NOT_RECORDING"
+    except Exception as e:
+        print(f"Error while stopping recording: {e}")
+        return "ERROR_STOPPING"
+
 
 # 카메라 자원 해제
 async def force_release_camera():
