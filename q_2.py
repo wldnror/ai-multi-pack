7dc77a22e06cb451079abf38c906bac72f559067
wldnror <72863449+wldnror@users.noreply.github.com>
2024-05-13 12:39:21 +0900

    Create q_2.py

---
+++ q_2.py
@@ -0,0 +1,15 @@
+import pika
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+# 큐에서 메시지를 받을 때마다 callback 함수를 호출
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
