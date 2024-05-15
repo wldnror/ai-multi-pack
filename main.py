f144bfd89abf4eb407524f22c6af2145e7ac34fa
wldnror <72863449+wldnror@users.noreply.github.com>
2024-05-15 19:11:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def send_status(sock, ip, port, message):
 
 def udp_server():
     udp_ip = "0.0.0.0"
-    udp_port = 12345
+    udp_port = 5000
     broadcast_ip = "255.255.255.255"
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
