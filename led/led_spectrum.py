abb9f9577264eef86f0829b7dbcc7174d6da601a
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-10 22:51:32 +0900

    Update led_spectrum.py

--- led/led_spectrum.py
+++ led/led_spectrum.py
@@ -57,7 +57,7 @@ def audio_callback(indata, frames, time, status):
 # 메인 함수
 def main():
     try:
-        with sd.InputStream(callback=audio_callback, channels=1, samplerate=SAMPLE_RATE, blocksize=1024, device='hw:4,0'):
+        with sd.InputStream(callback=audio_callback, channels=1, samplerate=SAMPLE_RATE, blocksize=1024, device='hw:4,1'):
             print("Streaming started...")
             while True:
                 time.sleep(1)
