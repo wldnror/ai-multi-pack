e7819b0557c2f0ed0d71f2630989c7c7d6b1a616
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-11 22:49:26 +0900

    Update b.py

--- TEST/bt/b.py
+++ TEST/bt/b.py
@@ -1,37 +1,11 @@
-import bluetooth
+import asyncio
+from bleak import BleakClient
 
-# UUID 설정
-uuid = "00001101-0000-1000-8000-00805F9B34FB"  # SPP (Serial Port Profile) UUID
+async def run(address):
+    async with BleakClient(address) as client:
+        model_number = await client.read_gatt_char("00002a24-0000-1000-8000-00805f9b34fb")
+        print("Model Number: {0}".format("".join(map(chr, model_number))))
 
-# 블루투스 서버 소켓 생성
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("", bluetooth.PORT_ANY))
-server_sock.listen(1)
-
-port = server_sock.getsockname()[1]
-
-# 서비스 광고 설정
-bluetooth.advertise_service(server_sock, "BluetoothServer",
-                            service_id=uuid,
-                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-                            profiles=[bluetooth.SERIAL_PORT_PROFILE])
-
-print(f"Waiting for connection on RFCOMM channel {port}")
-
-# 클라이언트 연결 수락
-client_sock, client_info = server_sock.accept()
-print(f"Accepted connection from {client_info}")
-
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if not data:
-            break
-        print(f"Received: {data}")
-except OSError:
-    pass
-
-print("Disconnected")
-
-client_sock.close()
-server_sock.close()
+address = "D8:3A:DD:3F:F8:84"  # 라즈베리파이의 MAC 주소
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(address))
