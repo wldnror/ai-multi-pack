b8108480085fcdfa320dbfff9e60f8dd099efe0c
wldnror <72863449+wldnror@users.noreply.github.com>
2024-06-12 21:11:13 +0900

    Update bluetooth_server.py

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -1,7 +1,6 @@
 from pydbus import SystemBus
 from gi.repository import GLib
 
-# 광고 데이터 설정
 class Advertisement:
     PATH = "/org/bluez/example/advertisement"
 
@@ -24,11 +23,14 @@ class Advertisement:
     def Release(self):
         pass
 
-# 광고 시작
 def register_advertisement(bus):
-    ad_manager = bus.get("org.bluez", "/org/bluez/hci0")
+    adapter = bus.get("org.bluez", "/org/bluez/hci0")
+    adapter_props = bus.get("org.freedesktop.DBus.Properties", "/org/bluez/hci0")
+    adapter_props.Set("org.bluez.Adapter1", "Powered", GLib.Variant("b", True))
+
+    ad_manager = bus.get("org.bluez.LEAdvertisingManager1", "/org/bluez/hci0")
     advertisement = Advertisement(bus)
-    ad_manager.LEAdvertisingManager1.RegisterAdvertisement(advertisement, {}, reply_handler=register_ad_cb, error_handler=register_ad_error_cb)
+    ad_manager.RegisterAdvertisement(advertisement, {}, reply_handler=register_ad_cb, error_handler=register_ad_error_cb)
     GLib.MainLoop().run()
 
 def register_ad_cb():
