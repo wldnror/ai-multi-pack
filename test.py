03a517a8f347480f87c3d201e5c257fec32eb1c0
wldnror <72863449+wldnror@users.noreply.github.com>
2024-08-03 19:52:04 +0900

    test.py 업데이트

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
-# GPIO 핀 설정 (물리적 핀 번호 기준)
+# GPIO 핀 설정 (BCM 번호 기준)
 red_led_pin = 24  # 빨간색 LED 핀 (BCM 24, 물리적 핀 18)
 green_led_pin = 23  # 초록색 LED 핀 (BCM 23, 물리적 핀 16)
+ground_pin = 23  # 가상 그라운드 핀 (BCM 23, 물리적 핀 16)
 
 # GPIO 모드 설정
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(red_led_pin, GPIO.OUT)
 GPIO.setup(green_led_pin, GPIO.OUT)
+GPIO.setup(ground_pin, GPIO.OUT)
+
+# 가상 그라운드 핀 항상 LOW로 설정
+GPIO.output(ground_pin, GPIO.LOW)
 
 # PWM 설정
 red_led_pwm = GPIO.PWM(red_led_pin, 100)  # 빨간색 LED 핀에서 100Hz 주파수로 PWM 설정
